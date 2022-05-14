0fc0bfce053d50e2e8df96299e6700c43748e5d0
Michael Gene Brockus <55331536+michael-gene-brockus@users.noreply.github.com>
2022-05-13 17:12:18 -0700

    Create main.cpp

---
+++ notebook/code-monk/02-monk-and-inversions/main.cpp
@@ -0,0 +1,21 @@
+//
+// HackerEarth folio
+// solved by Michael Gene Brockus
+//
+
+//
+// Overview:
+//     Monk's best friend Micro, who happen to be an awesome
+//     programmer, got him an integer matrix M of size  for
+//     his birthday. Monk is taking coding classes from Micro.
+//     They have just completed array inversions and Monk was
+//     successful in writing a program to count the number of
+//     inversions in an array. Now, Micro has asked Monk to
+//     find out the number of inversion in the matrix M. Number
+//     of inversions, in a matrix is defined as the number of
+//     unordered pairs of cells  such that .
+//
+//     Monk is facing a little trouble with this task and since
+//     you did not got him any birthday gift, you need to help 
+//     him with this task.
+//
